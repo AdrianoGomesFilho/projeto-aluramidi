4ad737a24bd8a174a1d1b8e080d4cedf4a273dd1
Adriano G <adrianogomesfilho98@gmail.com>
2023-05-11 20:00:13 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla'); /* Que será 9 */
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento);
+
+    const idAudio = '#som_{instrumento}'
+
+    tecla.onclick = function () {
+        tocaSom ('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
