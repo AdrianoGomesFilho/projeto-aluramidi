ab9132750d87187db01570bfe07730680fcb4b01
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2023-09-17 12:00:19 -0300

    .

--- Desafio/desafio.js
+++ Desafio/desafio.js
@@ -1,16 +1,27 @@
 
-function colocaNumeroTela (numeroInformado) {
-    document.querySelector('#visor').value += numeroInformado;
-  }
+// function colocaNumeroTela (numeroInformado) {
+//     document.querySelector('#visor').value += numeroInformado;
+//   }
   
-  const listaDeTeclas = document.querySelectorAll('input[type="button"]');
+// const listaDeTeclas = document.querySelectorAll('input[type="button"]');
   
-  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-    const tecla = listaDeTeclas[contador];
-    const numero = tecla.value;
+//   for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+//     const tecla = listaDeTeclas[contador];
+//     const numero = tecla.value;
     
-    tecla.onclick = function () {
-     colocaNumeroTela(numero);
-  }
-  }
+//     tecla.onclick = function () {
+//      colocaNumeroTela(numero);
+//   }
+//   }
   
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+  
+  const tecla = listaDeTeclas[indice];
+  
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  }
+}
