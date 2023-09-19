02925b22647c4fc90f3ebd293b59ea043b404f75
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2023-09-19 18:51:12 -0300

    .

--- Desafio/desafio.js
+++ Desafio/desafio.js
@@ -1,27 +1,15 @@
-
-// function colocaNumeroTela (numeroInformado) {
-//     document.querySelector('#visor').value += numeroInformado;
-//   }
-  
-// const listaDeTeclas = document.querySelectorAll('input[type="button"]');
-  
-//   for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-//     const tecla = listaDeTeclas[contador];
-//     const numero = tecla.value;
-    
-//     tecla.onclick = function () {
-//      colocaNumeroTela(numero);
-//   }
-//   }
-  
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
-const inputTel = document.querySelector('input[type=tel]');
 
-for (let indice = 0; indice < listaDeTeclas.length; indice++) {
-  
-  const tecla = listaDeTeclas[indice];
-  
-  tecla.onclick = function () {
-    inputTel.value = inputTel.value + tecla.value;
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+
+  tecla.onkeydown = function () {
+    tecla.classList.add('ativa');
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
   }
 }
